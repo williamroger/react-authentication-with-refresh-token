ec284ccd625accc047c46444dbaa2abfb7ccb157
William Roger <williamroger.frontend@gmail.com>
2026-05-05 01:09:59 -0300

    add auth-guard with layout

---
+++ src/Router/AuthGuard.tsx
@@ -0,0 +1,16 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+
+export function AuthGuard({ isPrivate }: { isPrivate: boolean }) {
+  const { signedIn } = useAuth();
+
+  if (signedIn && !isPrivate) {
+    return <Navigate to="/" replace />;
+  }
+
+  if (!signedIn && isPrivate) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  return <Outlet />;
+}
