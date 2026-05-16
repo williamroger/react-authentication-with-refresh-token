35d950c6ec47ca9ebef10351ddaed00bcdeff7aa
William Roger <williamroger.frontend@gmail.com>
2026-05-16 16:51:45 -0300

    add request interceptor for authorization header

--- src/services/HttpClient.ts
+++ src/services/HttpClient.ts
@@ -1,5 +1,16 @@
+import { StorageKeys } from '@/configs/StorageKeys';
 import axios from 'axios';
 
 export const HttpClient = axios.create({
   baseURL: 'http://localhost:3000',
+});
+
+HttpClient.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem(StorageKeys.accessToken);
+
+  if (accessToken) {
+    config.headers.set('Authorization', `Bearer ${accessToken}`);
+  }
+
+  return config;
 });
--- src/services/OrdersService.ts
+++ src/services/OrdersService.ts
@@ -4,13 +4,8 @@ import { StorageKeys } from '@/configs/StorageKeys';
 
 export class OrdersService {
   static async getOrders() {
-    const accessToken = localStorage.getItem(StorageKeys.accessToken);
 
-    const { data } = await HttpClient.get<{ orders: IOrder[] }>('/orders', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      }
-    });
+    const { data } = await HttpClient.get<{ orders: IOrder[] }>('/orders');
 
     return data.orders;
   }
