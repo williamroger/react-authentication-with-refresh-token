ae55c88ae963a37462968fe425d041eb2e676487
William Roger <williamroger.frontend@gmail.com>
2026-05-16 20:55:26 -0300

    moved request and response interceptors to auth-context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
-import { createContext, useCallback, useState } from 'react';
+import { createContext, useCallback, useLayoutEffect, useState } from 'react';
 import { AuthService } from '@/services/AuthService';
+import { HttpClient } from '@/services/HttpClient';
+import { StorageKeys } from '@/configs/StorageKeys';
 
 interface IAuthContextValue {
   signedIn: boolean;
@@ -37,6 +39,58 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     signIn,
     signOut,
   };
+
+  useLayoutEffect(() => {
+    const requestInterceptorId = HttpClient.interceptors.request.use((config) => {
+      const accessToken = localStorage.getItem(StorageKeys.accessToken);
+
+      if (accessToken) {
+        config.headers.set('Authorization', `Bearer ${accessToken}`);
+      }
+
+      return config;
+    });
+
+    return () => {
+      HttpClient.interceptors.request.eject(requestInterceptorId);
+    };
+  }, [])
+
+  useLayoutEffect(() => {
+    const responseInterceptorId = HttpClient.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        const originalRequest = error.config;
+        const refreshToken = localStorage.getItem(StorageKeys.refreshToken);
+
+        if (originalRequest.url === '/refresh-token') {
+          setSignedIn(false);
+          localStorage.clear();
+          return Promise.reject(error);
+        }
+
+        if ((error.response && error.response.status !== 401) || !refreshToken) {
+          return Promise.reject(error);
+        }
+
+        const {
+          accessToken,
+          refreshToken: newRefreshToken
+        } = await AuthService.refreshToken(refreshToken);
+
+        localStorage.setItem(StorageKeys.accessToken, accessToken);
+        localStorage.setItem(StorageKeys.refreshToken, newRefreshToken);
+
+        return HttpClient(originalRequest);
+      }
+    );
+
+    return () => {
+      HttpClient.interceptors.response.eject(responseInterceptorId);
+    };
+  }, []);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
--- src/services/HttpClient.ts
+++ src/services/HttpClient.ts
@@ -1,47 +1,5 @@
-import { StorageKeys } from '@/configs/StorageKeys';
 import axios from 'axios';
-import { AuthService } from './AuthService';
 
 export const HttpClient = axios.create({
   baseURL: 'http://localhost:3000',
 });
-
-HttpClient.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem(StorageKeys.accessToken);
-
-  if (accessToken) {
-    config.headers.set('Authorization', `Bearer ${accessToken}`);
-  }
-
-  return config;
-});
-
-HttpClient.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
-    const refreshToken = localStorage.getItem(StorageKeys.refreshToken);
-
-    if (originalRequest.url === '/refresh-token') {
-      window.location.href = '/sign-in';
-      localStorage.clear();
-      return Promise.reject(error);
-    }
-
-    if ((error.response && error.response.status !== 401) || !refreshToken) {
-      return Promise.reject(error);
-    }
-
-    const {
-      accessToken,
-      refreshToken: newRefreshToken
-    } = await AuthService.refreshToken(refreshToken);
-
-    localStorage.setItem(StorageKeys.accessToken, accessToken);
-    localStorage.setItem(StorageKeys.refreshToken, newRefreshToken);
-
-    return HttpClient(originalRequest);
-  }
-);
